ae3c140b4f744a8d4e84505f9f40d9dcf2539578
SenaiBruno <137070208+SenaiBruno@users.noreply.github.com>
2023-07-31 11:43:02 -0300

    Add files via upload
    adicionado o trabalho de casa

---
+++ OlaMundo/variavel.py
@@ -0,0 +1,12 @@
+# imput é uma funçao que permite perguntar algo ao usuario
+'''
+Bloco
+de
+Comentário
+Permite que  varias linhas se tornem um comentario
+'''
+
+nome = input("Qual é o seu nome?")
+
+print ("Ola", nome, ".Tudo bem com você?",)
+
